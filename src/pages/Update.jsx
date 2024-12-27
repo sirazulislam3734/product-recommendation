737ccbace65d8c65071ad2440f28eb74f53b5523
Riaz Uddin <sirazulislam3734@gmail.com>
2024-12-27 22:20:58 +0600

    added Extra section

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -139,7 +139,7 @@ const Update = () => {
             </div>
             <div className="flex justify-center w-full mt-6">
               <button className="disabled:cursor-not-allowed px-8 py-2.5 leading-5 text-white transition-colors duration-300 transhtmlForm bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-                Add Queries
+                Update Queries
               </button>
             </div>
           </form>
